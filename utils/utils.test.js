09b9ca4996287f7399ffac728c5812ed53c27a90
jayant sarkar <codejayant@gmail.com>
2017-10-14 14:57:54 -0700

    Mocha : basic setup and it function execution

---
+++ utils/utils.test.js
@@ -0,0 +1,17 @@
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  if (res !== 44) {
+    throw new Error(`Expected 44 but got ${res}`);
+  }
+});
+
+it('should square number', () => {
+  var square = utils.square(7);
+
+  if (square !== 49)  {
+    throw new Error(`Expected 49 but got ${square}`);
+  }
+});
