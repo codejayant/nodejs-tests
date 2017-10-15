925dbdcd9a86d42d56e5d326d1909a3139b48ded
jayant sarkar <codejayant@gmail.com>
2017-10-14 22:46:27 -0700

    added supertest to test express app

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ nodejs how to write test demo
 Understanding Mocha test for nodejs
 
 Command to enable test watcher using nodemon in terminal : <code>nodemon --exec "npm test"</code>
+<br/>
 To execute and run test watcher using npm script : <code>npm run test-watch</code>
+
+<p>supertest : https://github.com/visionmedia/supertest</p>
--- server/server.js
+++ server/server.js
@@ -3,7 +3,29 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!');
+  res.status(404).send({
+    error: 'Page not found',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([
+    {
+      name: 'jayant',
+      age: 29
+    },
+    {
+      name: 'maximus',
+      age: 35
+    },
+    {
+      name: 'black widow',
+      age: 30
+    }]);
 });
 
 app.listen(3000);
+
+// export app to be used by test
+module.exports.app = app;
