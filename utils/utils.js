1db86cdbec09437eea217e650c042187a02aa745
jayant sarkar <codejayant@gmail.com>
2017-10-14 19:27:25 -0700

    adding async test

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,19 @@ module.exports.add = (a, b) => {
   return a + b;
 };
 
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
+
 module.exports.square = (a) => a * a;
+
+module.exports.asyncSquare = (a, callback) => {
+  setTimeout(() => {
+    callback(a * a);
+  }, 1000);
+};
 
 module.exports.setName = (user, fullName) => {
   var names = fullName.split(' ');
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -7,10 +7,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(3, 4, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square number', () => {
   var square = utils.square(7);
 
   expect(square).toBe(49).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (square) => {
+    expect(square).toBe(25).toBeA('number');
+    done();
+  });
 });
 
 it('should have first and last name are set', () => {
